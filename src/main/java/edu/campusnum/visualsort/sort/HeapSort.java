2ff57239b11fabb0e086ead3eee7d59ece2609d1
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2023-08-02 11:53:49 +0200

    ADD TRI PAR TAS

--- src/main/java/edu/campusnum/visualsort/sort/HeapSort.java
+++ src/main/java/edu/campusnum/visualsort/sort/HeapSort.java
@@ -11,6 +11,48 @@ import edu.campusnum.visualsort.model.ObservableArray;
 public class HeapSort implements SortAlgorithm{
     @Override
     public void sort(ObservableArray array) {
-        throw new UnsupportedOperationException("Not implemented");
+        int longueur = array.getLength();
+
+        organiser(array);
+
+        for (int i = longueur-1; i > 0; i--) {
+            array.swap(0, i);
+            redescendre(array, i, 0);
+        }
+
+        //throw new UnsupportedOperationException("Not implemented");
+    }
+
+    public void organiser(ObservableArray array) {
+        int longueur = array.getLength();
+
+        for (int i = 0; i < longueur-1; i++) {
+            remonter(array, i);
+        }
+    }
+
+    public void remonter(ObservableArray array, int index) {
+        if (array.get(index) > array.get(index / 2)) {
+            array.swap(index, index/2);
+            remonter(array, index/2);
+        }
+    }
+
+    public void redescendre(ObservableArray array, int element, int index) {
+        int formule = 2 * index + 1;
+        int max;
+
+        if (formule < element) {
+            if (array.get(formule) > array.get(2 * index)) {
+                max = formule;
+            } else {
+                max = 2 * index;
+            }
+
+            if (array.get(max) > array.get(index)) {
+                array.swap(max, index);
+                redescendre(array, element, max);
+            }
+        }
     }
 }

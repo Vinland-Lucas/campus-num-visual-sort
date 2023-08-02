d9c9856e4d1fbc0a038d3dd4ada041ba07a62c47
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2023-08-02 16:24:17 +0200

    ADD TRI RAPIDE

--- src/main/java/edu/campusnum/visualsort/sort/QuickSort.java
+++ src/main/java/edu/campusnum/visualsort/sort/QuickSort.java
@@ -1,6 +1,7 @@
 package edu.campusnum.visualsort.sort;
 
 import edu.campusnum.visualsort.model.ObservableArray;
+import edu.campusnum.visualsort.model.Order;
 
 /**
  * Created by Web 74 all right reserved
@@ -11,6 +12,39 @@ import edu.campusnum.visualsort.model.ObservableArray;
 public class QuickSort implements SortAlgorithm{
     @Override
     public void sort(ObservableArray array) {
-        throw new UnsupportedOperationException("Not implemented");
+        int first = 0;
+        int length = array.getLength();
+        int last = length-1;
+
+        tri_rapide(array, first, last);
+
+        //throw new UnsupportedOperationException("Not implemented");
+    }
+
+    public void tri_rapide(ObservableArray array, int first, int last) {
+        //int length = array.getLength();
+
+        if (first < last) {
+            int pi = partitionner(array, first, last);
+            tri_rapide(array, first, pi-1);
+            tri_rapide(array, pi+1, last);
+        }
+    }
+
+    public int partitionner(ObservableArray array, int first, int last) {
+        int pivot = last;
+        int j = (first - 1);
+
+        for (int i = first; i <= pivot-1; i++) {
+            if (array.compare(pivot, i).equals(Order.Higher)) {
+                j = j + 1;
+                array.swap(i, j);
+
+            }
+        }
+
+        array.swap(j+ 1, pivot);
+
+        return (j + 1);
     }
 }
